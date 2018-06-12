db98c87795782a727409bd83241b7120db96f87c
daiki.ishibashi <daiki.a.ishibashi@rakuten.com>
2018-06-12 06:36:33 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
     
-Route::get('/', 'WelcomeController@index');    
+Route::get('/', 'WelcomeController@index');
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
     
 });
